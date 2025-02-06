9a87f0a1ff4cf14696a6f3790ce428655a8063d5
abhi1878 <abhishekyama1878@gmail.com>
2025-02-06 10:07:49 +0530

    my demo065 commit

---
+++ demo065.java
@@ -0,0 +1,11 @@
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        // Sample array
+        int[] array = {10, 20, 30, 40, 50};
+
+        // Loop through the array and print each element
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Element at index " + i + ": " + array[i]);
+        }
+    }
+}
