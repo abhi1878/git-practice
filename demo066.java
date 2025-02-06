b2a32132dff53a8405b0f756a776c16d34568719
abhi1878 <abhishekyama1878@gmail.com>
2025-02-06 10:08:41 +0530

    my demo066 commit

---
+++ demo066.java
@@ -0,0 +1,10 @@
+public class PrintArrayIndices {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        
+        System.out.println("Index values of the array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Index: " + i + ", Value: " + array[i]);
+        }
+    }
+}
